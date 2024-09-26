7af012893e4712c20095e6591d901d43f7485a74
JanekGraff <janek@rp-engineering.de>
2024-09-26 12:16:25 +0200

    Add initialization for I2C interface

--- src/i2c.rs
+++ src/i2c.rs
@@ -1,3 +1,5 @@
+use core::default;
+
 #[cfg(feature = "blocking")]
 use embedded_hal::i2c::I2c;
 #[cfg(feature = "async")]
@@ -5,19 +7,42 @@ use embedded_hal_async::i2c::I2c;
 
 use crate::Interface;
 
-struct I2CInterface<I2C>
-where
-    I2C: I2c,
-{
+#[derive(Debug, Default, Clone, Copy)]
+/// Possible slave addresses
+pub enum SlaveAddr {
+    /// Default
+    #[default]
+    Default,
+    /// Alternative slave address providing bit 0
+    Alternative(bool),
+}
+
+impl SlaveAddr {
+    fn addr(self) -> u8 {
+        const I2C_SLAVE_ADDR: u8 = 0b001_1000;
+        match self {
+            SlaveAddr::Default => I2C_SLAVE_ADDR,
+            SlaveAddr::Alternative(b) => I2C_SLAVE_ADDR | b as u8,
+        }
+    }
+}
+
+struct I2CInterface<I2C: I2c> {
     pub i2c: I2C,
     addr: u8,
 }
 
+impl<I2C: I2c> I2CInterface<I2C> {
+    pub fn new(i2c: I2C, addr: SlaveAddr) -> Self {
+        Self {
+            i2c,
+            addr: addr.addr(),
+        }
+    }
+}
+
 #[cfg(feature = "async")]
-impl<I2C> Interface for I2CInterface<I2C>
-where
-    I2C: I2c,
-{
+impl<I2C: I2c> Interface for I2CInterface<I2C> {
     type Error = I2C::Error;
 
     async fn write_read(&mut self, register: u8, buffer: &mut [u8]) -> Result<(), Self::Error> {
