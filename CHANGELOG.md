1c86aaa3b35b035b9964007e078a241d35e46006
JanekGraff <janek@rp-engineering.de>
2024-09-27 15:29:07 +0200

    implement Interface trait for SPIInterface

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ### Added
 
+- Implementation of Interface trait for embedded_hal\[_async\] v1.0 SpiDevice
+
 ### Fixed
 
 - Fix formatting of docstrings
--- src/spi.rs
+++ src/spi.rs
@@ -3,8 +3,6 @@ use embedded_hal::spi::SpiDevice;
 #[cfg(feature = "async")]
 use embedded_hal_async::spi::SpiDevice;
 
-use embedded_hal::digital::OutputPin;
-
 use crate::Interface;
 
 /// SPI interface for the driver
@@ -14,52 +12,49 @@ use crate::Interface;
 /// Using this wrapper struct instead of just an `embedded_hal::i2c::SpiDevice` we can easily support both
 /// I2C and SPI devices in the same driver.
 #[allow(unused)]
-pub struct SPIInterface<SPI: SpiDevice, CS: OutputPin> {
+pub struct SPIInterface<SPI: SpiDevice> {
     /// SPI device
     spi: SPI,
-    /// Chip select pin
-    cs: CS,
 }
 
-impl<SPI: SpiDevice, CS: OutputPin> SPIInterface<SPI, CS> {
+impl<SPI: SpiDevice> SPIInterface<SPI> {
     /// Create a new SPI interface from an SPI device and a chip select pin
     /// that implement the `SpiDevice` and `OutputPin` traits respectively.
     ///
     /// # Arguments
     /// * `spi` - SPI device
     /// * `cs` - Chip select pin
-    pub fn new(spi: SPI, cs: CS) -> Self {
-        Self { spi, cs }
+    pub fn new(spi: SPI) -> Self {
+        Self { spi }
     }
 }
 
 #[cfg(feature = "async")]
-impl<SPI: SpiDevice, CS: OutputPin> Interface for SPIInterface<SPI, CS> {
+impl<SPI: SpiDevice> Interface for SPIInterface<SPI> {
     type Error = SPI::Error;
 
     #[allow(unused_variables)]
     async fn write_read(&mut self, write: &[u8], read: &mut [u8]) -> Result<(), Self::Error> {
-        self.cs.set_low().ok();
-        todo!();
+        self.spi.transfer(read, write).await
     }
 
     #[allow(unused_variables)]
     async fn write(&mut self, data: &[u8]) -> Result<(), Self::Error> {
-        todo!()
+        self.spi.write(data).await
     }
 }
 
 #[cfg(feature = "blocking")]
-impl<SPI: SpiDevice, CS: OutputPin> Interface for SPIInterface<SPI, CS> {
+impl<SPI: SpiDevice> Interface for SPIInterface<SPI> {
     type Error = SPI::Error;
 
     #[allow(unused_variables)]
     fn write_read(&mut self, write: &[u8], read: &mut [u8]) -> Result<(), Self::Error> {
-        todo!()
+        self.spi.transfer(read, write)
     }
 
     #[allow(unused_variables)]
     fn write(&mut self, data: &[u8]) -> Result<(), Self::Error> {
-        todo!()
+        self.spi.write(data)
     }
 }
